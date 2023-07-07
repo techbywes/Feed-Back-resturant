9545d0c02030ed526591946f35557dd9c8c22c6e
techbywes <dhspotless@gmail.com>
2023-07-07 17:28:49 +0100

    updated the section2

--- src/componenets/Bestoftheday.js
+++ src/componenets/Bestoftheday.js
@@ -1,27 +1,43 @@
 import React from "react";
+import leftimage from "../images/bugger-left.png";
+import rightimage from "../images/drink-right.png";
 
 function Bestoftheday() {
   return (
     <section className="bestoftheday_div">
       <div className="left_div">
-        <div className="best_toopoint">Best of the day</div>
-        <h2 className="bestofhteday_discount_text">GET A 10% DISCOUNT ON CHRISTMAS</h2>
-        <p className="best_para_text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <button className="OrderNow_btn">
-            Order Now
-        </button>
+        <div className="general_div">
+          <div className="left_wrapup">
+            <div className="best_toopoint">Best of the day</div>
+            <h2 className="bestofhteday_discount_text">
+              GET A 10% DISCOUNT ON CHRISTMAS
+            </h2>
+            <p className="best_para_text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <button className="OrderNow_btn">Order Now</button>
+          </div>
+          <div className="left_image">
+            <img className="img_left" src={leftimage} alt="" />
+          </div>
+        </div>
       </div>
       <div className="right_div">
-      <div className="best_toopoint">Best of the day</div>
-        <h2 className="bestofhteday_discount_text">GET A 10% DISCOUNT ON CHRISTMAS</h2>
-        <p className="best_para_text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <button className="OrderNow_btn">
-        Order Now
-        </button>
+      <div className="general_div">
+          <div className="left_wrapup">
+            <div className="best_toopoint">Best of the day</div>
+            <h2 className="bestofhteday_discount_text">
+              GET A 10% DISCOUNT ON CHRISTMAS
+            </h2>
+            <p className="best_para_text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <button className="OrderNow_btn">Order Now</button>
+          </div>
+          <div className="left_image">
+            <img className="img_left" src={rightimage} alt="" />
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/componenets/Ourbestmeal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ourbestmeal() {
+  return (
+    <section className="ourbestmeal_div">
+      <h2 className="ourbest_meal_title_text">Our Best Menu</h2>
+      <p className="bestmeal_para_text">
+        All in good taste elementum integer enim neque volutpat ac.
+      </p>
+    </section>
+  );
+}
+
+export default Ourbestmeal;
