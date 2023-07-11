20c482d43ce53ef2564fb1f23a8152f49caeccb6
techbywes <dhspotless@gmail.com>
2023-07-11 01:30:40 +0100

    added registration form

--- src/componenets/ModelLinks.js
+++ src/componenets/ModelLinks.js
@@ -18,15 +18,21 @@ import {
 
 function ModelLinks() {
   const [showModel, setShowModel] = useState(false);
+  const [showMode2, setShowMode2] = useState(false);
 
   const handleLoginClick = () => {
     setShowModel(true);
   };
-
   const handleProfileIconClick = () => {
     setShowModel(false);
   };
 
+  const handleLoginClick2 = () => {
+    setShowMode2(true);
+  };
+  const handleProfileIconClick2 = () => {
+    setShowMode2(false);
+  };
   return (
     <div>
       <div className="model_link_gen_div">
@@ -81,7 +87,7 @@ function ModelLinks() {
 
                     <div className="text-center">
                       <p className="not_memeber_text">
-                        Not a member?{" "}
+                        Not a member?
                         <a href="#!" className="reg_text">
                           Register
                         </a>
@@ -114,9 +120,77 @@ function ModelLinks() {
             <div className="img_icon_div">
               <img className="icon_img" src={register} />
 
-              <a className="new_model_a" href="#">
+              <a className="new_model_a" href="#" onClick={handleLoginClick2}>
                 Register
               </a>
+              {showMode2 && (
+                <div className="login_form_div_">
+                  <img
+                    className="login_in_close_icon"
+                    src={close}
+                    onClick={handleProfileIconClick2}
+                  />
+                  <form>
+                    <MDBRow className="mb-4">
+                      <MDBCol>
+                        <MDBInput id="form3Example1" label="First name" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBInput id="form3Example2" label="Last name" />
+                      </MDBCol>
+                    </MDBRow>
+                    <MDBInput
+                      className="mb-4"
+                      type="email"
+                      id="form3Example3"
+                      label="Email address"
+                    />
+                    <MDBInput
+                      className="mb-4"
+                      type="password"
+                      id="form3Example4"
+                      label="Password"
+                    />
+
+                    <MDBCheckbox
+                      wrapperClass="d-flex justify-content-center mb-4"
+                      id="form3Example5"
+                      label="Subscribe to our newsletter"
+                      defaultChecked
+                    />
+
+                    <MDBBtn type="submit" className="mb-4" block>
+                      SIGN UP
+                    </MDBBtn>
+
+                    <div className="text-center">
+                      <p className="not_memeber_text">
+                        Not a member?{" "}
+                        <a href="#!" className="reg_text">
+                          Register
+                        </a>
+                      </p>
+                      <p className="sign_up_text_">or sign up with:</p>
+
+                      <MDBBtn floating color="secondary" className="mx-1">
+                        <MDBIcon fab icon="facebook-f" />
+                      </MDBBtn>
+
+                      <MDBBtn floating color="secondary" className="mx-1">
+                        <MDBIcon fab icon="google" />
+                      </MDBBtn>
+
+                      <MDBBtn floating color="secondary" className="mx-1">
+                        <MDBIcon fab icon="twitter" />
+                      </MDBBtn>
+
+                      <MDBBtn floating color="secondary" className="mx-1">
+                        <MDBIcon fab icon="github" />
+                      </MDBBtn>
+                    </div>
+                  </form>
+                </div>
+              )}
             </div>
           </li>
 
