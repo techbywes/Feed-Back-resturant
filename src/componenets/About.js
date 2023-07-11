39ed0dbb7fc57cdf35fd2ed1040d4c68f78e59a3
techbywes <dhspotless@gmail.com>
2023-07-11 22:42:55 +0100

    updated the about us page

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -3,6 +3,9 @@ import Nav from "./Nav";
 import bugaIMG from "../images/bugger.png";
 import signature from "../images/signature.png";
 import bugger2 from "../images/bugger2.png";
+import Footer from "./Footer";
+import phone from "../images/phone.png";
+import resturant from "../images/restaurant.png";
 
 function About() {
   return (
@@ -64,7 +67,7 @@ function About() {
               Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
               sem. Nulla consequat massa quis enim.
             </p>
-            <img className="_signature_"  src={signature} />
+            <img className="_signature_" src={signature} />
             <h4 className="author_name_">George Shaw, CEO</h4>
           </div>
 
@@ -72,7 +75,69 @@ function About() {
             <img className="img_signature_" src={bugger2} />
           </div>
         </section>
+        <section className="general_section_div">
+          <div className="need_help_div_">
+            <div className="left_text_check_food_">
+              <div className="left_text_div_">
+                <h2 className="good_food_text_">
+                  Check Your Food Court Restaurant location.
+                </h2>
+                <div className="line_div"></div>
+                <p className="good_food_orar_text">
+                  All in good taste elementum integer enim neque volutpat ac.
+                  Morbi enim nunc faucibus a. Amet consectetur adipiscing elit
+                  duis tempus urna et pharetra. Sagittis scelerisque purus
+                  semper eget duis. Viverra maecenas accumsan lacus vel
+                  facilisis volutpat est. Ultricies lacus sed turpis tincidunt
+                  id aliquet risus feugiat in.
+                </p>
+                <button className="location_btn_">Check Our Location</button>
+              </div>
+            </div>
+            <div className="right_need_help_div">
+              <div className="help_address_div__">
+                <div className="need_help_mail_div">
+                  <div className="need_help_text">NEED HELP?</div>
+                  <div className="call_resturant_">CALL US RESTAURANT</div>
+                </div>
+                <div className="need_help_mail_div">
+                  <div className="phone_phone_number">
+                    <div className="phone_icon_div">
+                      <img className="phone_icon" src={phone} />
+                    </div>
+                    <div className="need_help_text">+123-234-1234</div>
+                  </div>
+                  <div className="mail_text">admin@example.com</div>
+                </div>
+              </div>
+              <div className="adrresses_div_">
+                <div className="adress_icon_div__">
+                  <img className="resturant_icon_" src={resturant} />
+                  Feed Back Post Restaurant 1 - Address, Fax 99 Roving St, Big
+                  City PKU
+                </div>
+              </div>
+              <div className="dotted_line_div"></div>
+              <div className="adrresses_div_">
+                <div className="adress_icon_div__">
+                  <img className="resturant_icon_" src={resturant} />
+                  Feed Back Post Restaurant 1 - Address, Fax 99 Roving St, Big
+                  City PKU
+                </div>
+              </div>
+              <div className="dotted_line_div"></div>
+              <div className="adrresses_div_">
+                <div className="adress_icon_div__">
+                  <img className="resturant_icon_" src={resturant} />
+                  Feed Back Post Restaurant 1 - Address, Fax 99 Roving St, Big
+                  City PKU
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
+      <Footer />
     </>
   );
 }
